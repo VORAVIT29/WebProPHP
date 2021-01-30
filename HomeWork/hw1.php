b255aec02d7f5d33a96da8cb9bb3a9002984e907
VORAVIT29 <6206021610153@fitm.kmutnb.ac.th>
2021-01-30 18:21:01 +0700

    update

--- HomeWork/hw1.php
+++ HomeWork/hw1.php
@@ -12,7 +12,7 @@
     </style>
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['btnsub'])) {
         $sum = '';
         $num1 = $_POST['num1'];
         $num2 = $_POST['num2'];
@@ -30,6 +30,11 @@
                     break;
             }
         }
+    } else {
+        $_SERVER['PHP_SELF'];
+        $num1 = "";
+        $num2 = "";
+        $num3 = "";
     }
     ?>
 </head>
@@ -44,22 +49,22 @@
         <div>
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <label>Num 1: </label>
-                <input type="number" name="num1" style="width: 20%;" required>
+                <input type="text" name="num1" style="width: 20%;" required>
                 <br><br><label>Num 2: </label>
-                <input type="number" name="num2" style="width: 20%;" required>
+                <input type="text" name="num2" style="width: 20%;" required>
                 <br><br><label>Num 3: </label>
-                <input type="number" name="num3" style="width: 20%;" required>
+                <input type="text" name="num3" style="width: 20%;" required>
                 <br><br><input type="radio" name="choice" value="Minnimum" required>Minnimum
                 <input type="radio" name="choice" value="Maximum">Maximum
                 <input type="radio" name="choice" value="Average">Average
-                <br><br><button type="submit">Submit</button>
+                <br><br><button type="submit" name="btnsub">Submit</button>
                 <button type="reset">Clear</button>
             </form>
         </div>
         <br><br>
         <?php
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            echo $_POST['choice'] . " number = $sum";
+        if (isset($_POST['btnsub'])) {
+            echo $_POST['choice'] . " number = " . number_format($sum);
         }
         ?>
     </center>
